937963cddd74815ad46d60a55cb66f8d884819fe
VishikBhalla <vishik.bhalla@gmail.com>
2019-10-28 17:14:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-
-var mysql = require('mysql');
+var mysql      = require('mysql');
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root"
+  host     : 'us-cdbr-iron-east-05.cleardb.net',
+  user     : 'b912905bc8ad2c',
+  password : '1a5c658a',
+  database : 'heroku_1549d99746341be'
 });
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 });
-
 
 const express = require('express')
 const app = express();
