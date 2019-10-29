6974e31605ac13f5f9b047e121ec72373f8b948c
Alek Piekarski <piekarski.a@husky.neu.edu>
2019-10-29 00:08:18 -0400

    Remove unnecessary command

--- index.js
+++ index.js
@@ -17,10 +17,6 @@ const express = require('express')
 const app = express();
 
 app.get('/', (req, res) => {
-    con.query('CREATE TABLE ingredient (\n' +
-        '    ingredient_id INT PRIMARY KEY,\n' +
-        '    ingredient_name VARCHAR(30) NOT NULL\n' +
-        ')');
     con.query('INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (1, `rum`), (2, `gin`), (3, `tequila`), (4, `whiskey`), (5, `vodka`)');
     var query = con.query('SELECT ingredient_name FROM ingredient');
     res.send(query);
